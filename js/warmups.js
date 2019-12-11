baa046421d9c2cf4e6ea0926d1f03702cfd66960
jasonwise <jasonandrewwise@gmail.com>
2019-12-10 21:48:19 -0500

    creating new problems to try to solve

--- js/warmups.js
+++ js/warmups.js
@@ -1,6 +1,8 @@
 // Take an array of names and add a last name to them
 const firstNames = ["Jason", "Jack", "Terry", "Lester"];
 
+const numbers = [1, 2, 3, 4, 5];
+
 // Use an arrow Function to map over the values in first name and add wise to them
 // Arrow functions use implicit returns -- with these types of callback functions where
 // we just return something immediately we can do that in one line -- therefore there is no
@@ -23,3 +25,19 @@ firstNames.map(name => console.log(name));
 
 // Console log each first name on a seperate line again...
 firstNames.map(name => console.log(name));
+
+// Sum the numbers array
+
+// Find the max of numbers array
+
+// find the min of the numbers array
+
+// find the average of the numbers array
+
+// show only even numbers
+
+// show only odd numbers
+
+// first odd number
+
+// first even number
